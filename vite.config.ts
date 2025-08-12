a4a9e5aa008371d0aec54fd92aced42681e9b78f
amandollar <toxicempire3@gmail.com>
2025-08-12 18:23:37 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),react()],
+  plugins: [react(), tailwindcss()],
+  base: '/',  // Ensures correct asset loading
+  build: {
+    outDir: 'dist',  // Ensures output directory is correct
+  }
 })
